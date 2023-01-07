d33f2ad10f79a748d74ab4c06943b2b0d6f30d5c
Ali <34485195+aliovice@users.noreply.github.com>
2023-01-07 23:23:21 +0100

    b commit

---
+++ frontend/build/precache-manifest.148e35c8a6f3abb085ab7b04c8809a83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0662a871b39e94feea9c",
+    "url": "/static/css/main.e8c526a3.chunk.css"
+  },
+  {
+    "revision": "0662a871b39e94feea9c",
+    "url": "/static/js/main.0662a871.chunk.js"
+  },
+  {
+    "revision": "032d891be1b2709f7244",
+    "url": "/static/js/1.032d891b.chunk.js"
+  },
+  {
+    "revision": "f8915e893a24733d4edb",
+    "url": "/static/css/2.b232c31e.chunk.css"
+  },
+  {
+    "revision": "f8915e893a24733d4edb",
+    "url": "/static/js/2.f8915e89.chunk.js"
+  },
+  {
+    "revision": "8235a4a162a1089cfaa8",
+    "url": "/static/js/runtime~main.8235a4a1.js"
+  },
+  {
+    "revision": "fd3f481180f8a7308e6b0fc0c39792b3",
+    "url": "/index.html"
+  }
+];
